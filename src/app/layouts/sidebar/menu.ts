d394aec7cc962b09c07e2913b9c4c5aaaadd34de
Absalon2327 <cm16057@ues.edu.sv>
2023-07-15 22:03:18 -0600

    primer Commit

---
+++ src/app/layouts/sidebar/menu.ts
@@ -0,0 +1,23 @@
+import { MenuItem } from './menu.model';
+
+export const MENU: MenuItem[] = [
+    {
+        id: 1,
+        label: 'MENUITEMS.MENU.TEXT',
+        isTitle: true
+    },
+    {
+        id: 2,
+        label: 'Inicio',
+        icon: 'bx-home-circle',
+        link: '/dashboard',
+
+    },
+
+    {
+        id: 3,
+        label: 'Modulos',
+        isTitle: true
+    },
+];
+
